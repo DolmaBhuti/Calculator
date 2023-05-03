6e2c2f24739deba0d6c21543ca0ac5cbdf6b3321
Dolma Bhuti <dbhuti95@gmail.com>
2023-05-03 16:14:43 -0400

    all functions done; created styles

--- src.js
+++ src.js
@@ -1,9 +1,10 @@
-operandA = "";
-operandB = "";
-operator = "";
+let operandA = "";
+let operandB = "";
+let operator = "";
 let displayValue = "0";
 const keys = document.querySelector(".keys");
 const displayP = document.querySelector("#display-value");
+const keyList = document.querySelectorAll(".key");
 
 function add(opA, opB) {
   return opA + opB;
@@ -25,7 +26,12 @@ function operate(operator, opA, opB) {
     result = multiply(opA, opB);
   }
   else if (operator.includes("divide")) {
-    result = divide(opA, opB);
+    if (opA === 0) {
+      result = "Nope";
+    }
+    else {
+      result = divide(opA, opB);
+    }
   }
   else if (operator.includes("add")) {
     result = add(opA, opB);
@@ -35,7 +41,31 @@ function operate(operator, opA, opB) {
   }
   displayValue = "";
   console.log(opA + " " + operator + " " + opB + " " + result);
-  displayNumber(result);
+  const result_str = result.toString();
+  if (result_str.length <= 19) {    //limit number of characters in display
+    if (result_str.includes(".")) {
+      /**
+       * TODO: round decimal numbers 
+       */
+      //check how many numbers AFTER decimal there are 
+      const rounded = result_str.split(".");
+      const length = rounded[1].length;
+
+      //use toFixed to round and update the display
+      const fixed = parseFloat(result).toFixed(length);
+
+      result_str = fixed.toString();
+    }
+    displayNumber(result_str);
+  }
+  else {
+    result_str = result_str.substring(0, 19);
+    // //TODO: if result is too long
+    // const length = result_str.slice(-2);
+    // console.log("length: " + length);
+    // result_str = 1 + "e" + (parseInt(length) + 1);
+    displayNumber(result_str);
+  }
 }
 //Create the functions that populate the display when you click the number buttons.
 //add event listener for when someone clicks a number button
@@ -44,29 +74,30 @@ function displayNumber(value) {
   if (displayValue === "0" || displayValue === "") {
     displayValue = value;
   } else {
-    //else -> append textContent to the displayContent
-    if (value === "." && displayValue.includes(".")) {  //cant have 2 decimals
-      displayValue = displayValue;
-    }
-    else {
-      if (displayValue.length <= 10) {
+    if (displayValue.length < 19) {  //limit number of characters in display
+      //else -> append textContent to the displayContent
+      if (value === "." && displayValue.includes(".")) {  //cant have 2 decimals
+        displayValue = displayValue;
+      }
+      else {
         displayValue = displayValue + value;
       }
     }
-  }
-  //round displayvalue with long decimals so that they don’t overflow the screen
 
+  }
   displayP.textContent = displayValue;
+  console.log(displayValue);
 }
 displayNumber("0");
-//when a number is clicked, display it
 
 keys.addEventListener('click', (event) => {
   const key = event.target.className;
   const value = event.target.textContent;
 
+
+
   //if number is clicked, append it to the displayValue until the operator/equal/clear button is clicked
-  if (key === "number") {
+  if (key.includes("number")) {
     displayNumber(value);
   }
   else if (key.includes("operator")) { //if operator is clicked, then make the first operand the displayValue (if its empty)
@@ -76,22 +107,31 @@ keys.addEventListener('click', (event) => {
     //and finally, use that result (19) as the first number in your new calculation, along with the next operator (-).
     if (operandA != "" && operator != "" && displayValue != "") {  //only oprate if all 3 variables are populated
       operandB = displayValue;
-      operate(operator, parseFloat(operandA), parseFloat(operandB));
+      operate(operator, parseFloat(operandA), parseFloat(displayValue));
     }
-    operandA = displayValue;
+    if ((displayValue != "0" || displayValue != "") && operandA == "") { //only update first oeprand if there is no first operand (operandA), AND user has pressed a numer 
+      operandA = displayValue;
+    }
     operator = key;
     displayValue = "";
+    console.log(operator)
+    console.log(key)
+    console.log("operandA" + operandA)
+
   }
-  else if (key === "equal-btn") {
+  else if (key.includes("equal")) {
     console.log("equal button pressed");
     if (displayValue != "" && operandA != "" && operator != "") {  //only oprate if all 3 variables are populated
       operandB = displayValue;
       operate(operator, parseFloat(operandA), parseFloat(operandB));
       operator = "";
+      operandA = displayValue;   //make the result the first oeprand of the next operation
+      operandB = "";
     }
   }
-  else if (key === "clear-btn") {
+  else if (key.includes("clear-btn")) {
     displayValue = "0";
+    //value = "0"
     operandA = "";
     operandB = "";
     operator = "";
@@ -99,11 +139,39 @@ keys.addEventListener('click', (event) => {
   }
 });
 
-/**
- * 1. user presses numbers
- * - save the number in the displayValue
- * 2. user presses operator
- * - save the operator in the operator variable
- * IF there are two operands already -> operate on it
- * ELSE dont do anything
- */
+//when any key is clicked over an element, change the background color 
+keys.addEventListener('mousedown', (event) => {
+  const key = event.target.className;
+
+  if (key.includes("number")) {
+    event.target.classList.replace("number", "key-pressed");
+
+  }
+  else if (key.includes("operator")) {
+    event.target.classList.replace("operator", "key-pressed");
+  }
+  else if (key.includes("equal")) {
+    event.target.classList.replace("equal-btn", "key-pressed");
+  }
+  else if (key.includes("clear-btn")) {
+    //event.target.classList.replace("c", "key-pressed");
+    //event.target.style.cssText = "background-color: #8294C4;"
+  }
+});
+//when mouse is clicked up
+keys.addEventListener('mouseup', (event) => {
+  const key = event.target.className;
+
+  if (key.includes("n")) {
+    event.target.classList.replace("key-pressed", "number");
+  }
+  else if (key.includes("o")) {
+    event.target.classList.replace("key-pressed", "operator");
+  }
+  else if (key.includes("e")) {
+    event.target.classList.replace("key-pressed", "equal-btn");
+  }
+  else if (key.includes("clear-btn")) {
+    //event.target.style.cssText = "background-color: #AFD3E2;"
+  }
+});
